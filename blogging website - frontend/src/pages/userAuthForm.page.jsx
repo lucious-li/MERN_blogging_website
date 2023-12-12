0b205c097b246890a3919f4aee2ab4eff2a711e2
peiyangli <li-peiyang@outlook.com>
2023-12-12 23:59:21 +0800

    Complete the server-side login and registration logic and the generation and verification of jwt, and complete the connection to mongodb database and the storage of user login information.

--- blogging website - frontend/src/pages/userAuthForm.page.jsx
+++ blogging website - frontend/src/pages/userAuthForm.page.jsx
@@ -1,71 +1,74 @@
 import { Link } from "react-router-dom";
 import InputBox from "../components/input.component";
 import googleIcon from "../imgs/google.png";
+import AnimationWrapper from "../common/page-animation";
 
 const UserAuthForm = ({ type }) => {
   return (
-    <section className="h-cover flex items-center justify-center ">
-      <form className="w-[80%] max-w-[400px]">
-        <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
-          {type == "sign-in" ? "Welcome" : "Join us today"}
-        </h1>
+    <AnimationWrapper keyValue={type}>
+      <section className="h-cover flex items-center justify-center ">
+        <form className="w-[80%] max-w-[400px]">
+          <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
+            {type == "sign-in" ? "Welcome" : "Join us today"}
+          </h1>
 
-        {type != "sign-in" ? (
+          {type != "sign-in" ? (
+            <InputBox
+              name="fullname"
+              type="text"
+              placeholder="Full name"
+              icon="fi-rs-circle-user"
+            />
+          ) : (
+            ""
+          )}
+
           <InputBox
-            name="fullname"
-            type="text"
-            placeholder="Full name"
-            icon="fi-rs-circle-user"
+            name="email"
+            type="email"
+            placeholder="Email"
+            icon="fi-rr-envelope"
           />
-        ) : (
-          ""
-        )}
+          <InputBox
+            name="password"
+            type="password"
+            placeholder="Password"
+            icon="fi-rs-key"
+          />
 
-        <InputBox
-          name="email"
-          type="email"
-          placeholder="Email"
-          icon="fi-rr-envelope"
-        />
-        <InputBox
-          name="password"
-          type="password"
-          placeholder="Password"
-          icon="fi-rs-key"
-        />
+          <button className="btn-dark center mt-14" type="submit">
+            {type.replace("-", " ")}
+          </button>
 
-        <button className="btn-dark center mt-14" type="submit">
-          {type.replace("-", " ")}
-        </button>
+          <div className="relative w-full flex items-center gap-2 my-10 opacity-10 uppercase text-black font-bold">
+            <hr className="w-1/2 border-black" />
+            <p>or</p>
+            <hr className="w-1/2 border-black" />
+          </div>
 
-        <div className="relative w-full flex items-center gap-2 my-10 opacity-10 uppercase text-black font-bold">
-          <hr className="w-1/2 border-black" />
-          <p>or</p>
-          <hr className="w-1/2 border-black" />
-        </div>
+          <button className="btn-dark flex  items-center justify-center gap-4 w-[90%] center">
+            <img src={googleIcon} className="w-5" />
+            continue with Google
+          </button>
 
-        <button className="btn-dark flex  items-center justify-center gap-4 w-[90%] center">
-          <img src={googleIcon} className="w-5" />
-          continue with Google
-        </button>
-
-        {type == "sign-in" ? (
-          <p className="mt-6 text-dark-grey text-xl text-center">
-            Don't have an account ?
-            <Link to="/signup" className="underline text-black text-xl ml-1">
-              Join us today
-            </Link>
-          </p>
-        ) : (
-          <p className="mt-6 text-dark-grey text-xl text-center">
-            Already a member ?
-            <Link to="/signin" className="underline text-black text-xl ml-1">
-              Sign in here
-            </Link>
-          </p>
-        )}
-      </form>
-    </section>
+          {type == "sign-in" ? (
+            <p className="mt-6 text-dark-grey text-xl text-center">
+              Don't have an account ?
+              <Link to="/signup" className="underline text-black text-xl ml-1">
+                Join us today
+              </Link>
+            </p>
+          ) : (
+            <p className="mt-6 text-dark-grey text-xl text-center">
+              Already a member ?
+              <Link to="/signin" className="underline text-black text-xl ml-1">
+                Sign in here
+              </Link>
+            </p>
+          )}
+        </form>
+      </section>
+    </AnimationWrapper>
   );
 };
 export default UserAuthForm;
---
+++ server/server.js
@@ -0,0 +1,115 @@
+import express from "express";
+import mongoose from "mongoose";
+import "dotenv/config";
+import bcrypt from "bcrypt";
+import { nanoid } from "nanoid";
+import jwt from "jsonwebtoken";
+
+//schema below
+import User from "./Schema/User.js";
+
+const server = express();
+let PORT = 3000;
+
+let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
+let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
+
+server.use(express.json());
+
+mongoose.connect(process.env.DB_LOCATION, {
+  autoIndex: true,
+});
+
+const formatDatattoSend = (user) => {
+  const access_token = jwt.sign(
+    { id: user._id },
+    process.env.SECRET_ACCESS_KEY
+  );
+  return {
+    access_token,
+    profile_img: user.personal_info.profile_img,
+    username: user.personal_info.username,
+    fullname: user.personal_info.fullname,
+  };
+};
+
+const generateUsername = async (email) => {
+  let username = email.split("@")[0];
+  let usernameExists = await User.exists({
+    "personal_info.username": username,
+  });
+  usernameExists ? (username += nanoid().substring(0, 5)) : "";
+  return username;
+};
+
+server.post("/signup", (req, res) => {
+  let { fullname, email, password } = req.body;
+  //validating the data from frontend
+  if (fullname.length < 3) {
+    return res
+      .status(403)
+      .json({ error: "Fullname must be at least 3 letters long" });
+  }
+  if (!email.length) {
+    return res.status(403).json({ error: "enter email" });
+  }
+  if (!emailRegex.test(email)) {
+    return res.status(403).json({ error: "email is invalid" });
+  }
+  if (!passwordRegex.test(password)) {
+    return res.status(403).json({
+      error:
+        "password should to be 6 to 20 characters long with numeric, 1 lowercase and 1 uppercase letters",
+    });
+  }
+  bcrypt.hash(password, 10, async (err, hashed_password) => {
+    let username = await generateUsername(email);
+    let user = new User({
+      personal_info: { fullname, email, password: hashed_password, username },
+    });
+
+    user
+      .save()
+      .then((u) => {
+        return res.status(200).json(formatDatattoSend(u));
+      })
+
+      .catch((err) => {
+        if (err.code == 11000) {
+          return res.status(500).json({ error: "Email already exist" });
+        }
+        return res.status(500).json({
+          error: err.message,
+        });
+      });
+  });
+});
+
+server.post("/signin", (req, res) => {
+  let { email, password } = req.body;
+  User.findOne({ "personal_info.email": email })
+    .then((user) => {
+      if (!user) {
+        return res.status(403).json({ error: "Email not found" });
+      }
+      bcrypt.compare(password, user.personal_info.password, (err, result) => {
+        if (err) {
+          return res
+            .status(403)
+            .json({ error: "Error occured while login please try again" });
+        }
+        if (!result) {
+          return res.status(403).json({ error: "incorrect password" });
+        } else {
+          return res.status(200).json(formatDatattoSend(user));
+        }
+      });
+    })
+    .catch((err) => {
+      console.log(err.message);
+      return res.status(500).json({ error: "err.message" });
+    });
+});
+server.listen(PORT, () => {
+  console.log("listening on port ->" + PORT);
+});
