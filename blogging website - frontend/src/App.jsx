1e857288b00997433ab74c1133512da29c7dbcec
peiyangli <li-peiyang@outlook.com>
2023-12-12 13:19:56 +0800

    Complete the input and userAuthForm components

--- blogging website - frontend/src/App.jsx
+++ blogging website - frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar.component";
+import UserAuthForm from "./pages/userAuthForm.page";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navbar />}>
-        <Route path="signin" element={<h1>sign in page</h1>} />;
-        <Route path="signup" element={<h1>sign up page</h1>} />;
+        <Route path="signin" element={<UserAuthForm type="sign-in" />} />;
+        <Route path="signup" element={<UserAuthForm type="sign-up" />} />;
       </Route>
     </Routes>
   );
